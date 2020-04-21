4de5b43e7047170b67f8e481c4a54568df5dd9ad
Tpierga <thibaultpierga@outlook.fr>
2020-04-21 11:39:34 +0200

    adding box collider on car

---
+++ Projet Dectection IA/Assets/controllerOmni.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllerOmni : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * 20));
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.up);
+        }
+    }
+}
