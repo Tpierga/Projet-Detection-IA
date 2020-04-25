0f6e5c288af6d3974bc68fba0d41e8a563cdc7c2
Tpierga <thibaultpierga@outlook.fr>
2020-04-25 18:14:12 +0200

    adding snapshots when press -Space

--- Projet Dectection IA/Assets/controllerOmni.cs
+++ Projet Dectection IA/Assets/controllerOmni.cs
@@ -4,26 +4,45 @@ using UnityEngine;
 
 public class controllerOmni : MonoBehaviour
 {
+    public SnapShotCamera snapCam;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            snapCam.TakeSnapShot();
+        }
+        Forward();
+        Rotate();
+    }
+
+    void Forward()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * (Time.deltaTime * 20));
+            transform.Translate(-Vector3.forward * ((Time.deltaTime) * 2));
         }
-        else if (Input.GetKey(KeyCode.Q))
+        else if (Input.GetKey(KeyCode.S))
         {
-            transform.Rotate(Vector3.up);
+            transform.Translate(Vector3.forward * ((Time.deltaTime) * 2));
+        } 
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.up);
+            transform.Rotate(Vector3.up);
         }
     }
 }
