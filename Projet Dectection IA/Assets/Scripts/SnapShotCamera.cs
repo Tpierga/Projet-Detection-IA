5979c4595295d0c0f1c6716ea6bf56badbeaf1d8
Tpierga <thibaultpierga@outlook.fr>
2020-05-12 15:06:40 +0200

    adding possibility to send image through Prorok server solution

--- Projet Dectection IA/Assets/Scripts/SnapShotCamera.cs
+++ Projet Dectection IA/Assets/Scripts/SnapShotCamera.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
@@ -10,6 +11,7 @@ public class SnapShotCamera : MonoBehaviour
 
     private int resWidth = 455;
     private int resHeight = 256;
+    
 
     private void Awake()
     {
@@ -32,7 +34,24 @@ public class SnapShotCamera : MonoBehaviour
         snapCam.gameObject.SetActive(true);
     }
 
-    void LateUpdate()
+    public byte[] EncodeImage()
+    {
+        byte[] bytes = new byte[]{}; 
+        
+        if (snapCam.gameObject.activeInHierarchy)
+        {
+            Texture2D snapshot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            snapCam.Render();
+            RenderTexture.active = snapCam.targetTexture;
+            snapshot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            bytes = snapshot.EncodeToJPG();
+            snapCam.gameObject.SetActive(false);
+        }
+        return bytes;
+        
+    }
+
+    public void SaveImageToFile()
     {
         if (snapCam.gameObject.activeInHierarchy)
         {
@@ -48,7 +67,7 @@ public class SnapShotCamera : MonoBehaviour
             snapCam.gameObject.SetActive(false);
         }
     }
-
+    
     string SnapshotName()
     {
         return string.Format("{0}/Snapshots/snap_{1}x{2}_{3:yyyy-MM-dd_HH-mm-ss}.jpg", Application.dataPath, resWidth, resHeight, System
